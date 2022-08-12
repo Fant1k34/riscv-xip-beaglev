0e00dff9821e3c614e6fb6c71578360804472e26
come1llf00 <rayveleaveme34@inbox.ru>
2022-08-12 20:02:05 +0300

    feat: optimized building tarballs

--- pack-kernel.sh
+++ pack-kernel.sh
@@ -2,5 +2,5 @@
 
 KERNEL_VER=$1
 
-rm -rf "$KERNEL_VER.tar.xz"
-tar --exclude-vcs -Jcvf "$KERNEL_VER.tar.xz" $KERNEL_VER
+# rm -rf "$KERNEL_VER.tar.xz"
+tar --exclude-vcs --update -vf "$KERNEL_VER.tar" $KERNEL_VER
